06c990618e14deeb4a101deb51c125d8ec036f64
Tyler Lutz <tyler@tylerlutz.com>
2018-09-19 22:05:21 -0400

    Initial Commit

---
+++ LearningWebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using LearningWebAPI.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LearningWebAPI.Model
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    client.BaseAddress = new Uri("https://reqres.in/");
+                    var response = await client.GetAsync("/api/users?page=2");
+                    response.EnsureSuccessStatusCode();
+
+                    var stringResult = await response.Content.ReadAsStringAsync();
+                    var rawData = JsonConvert.DeserializeObject<UsersList>(stringResult);
+                    return Ok(new
+                    {
+                        Page = rawData.page,
+                        PerPage = rawData.per_page,
+                        Total = rawData.total,
+                        TotalPages = rawData.total_pages,
+                        Data = rawData.data
+                    });
+                }
+                catch (HttpRequestException exception)
+                {
+                    return BadRequest($"Error getting weather from OpenWeather: {exception.Message}");
+                }
+            }
+        }
+    }
+}
---
+++ LearningWebAPI/Model/UsersList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace LearningWebAPI.Model
+{
+    public class UsersList
+    {
+        public int page { get; set; }
+        public int per_page { get; set; }
+        public int total {get; set; }
+        public int total_pages { get; set; }
+        public IEnumerable<DataModel> data { get; set; }
+    }
+
+    public class DataModel
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string avatar { get; set; }
+    }
+}
